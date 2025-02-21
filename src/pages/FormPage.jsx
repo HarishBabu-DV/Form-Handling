6723a59c1bba65dbd66c6c96695093024f2bfa11
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-02-21 21:59:33 +0530

    modified input width

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -35,32 +35,32 @@ const FormPage = () => {
             
             <div className="flex items-center px-4">
               <Label inputName={'name'} labelName={'name'} className={'capitalize'}/>     
-              <input type="text" name="name" id="" className="border-1 rounded-sm px-4 py-2 w-[60%]" />
+              <input type="text" name="name" id="" className="border-1 rounded-sm px-4 py-2 w-[80%]" />
             </div>
             
             <div className="flex items-center px-4">
               <Label inputName={'description'} labelName={'description'} className={'capitalize'}/>    
-              <textarea name="description" id="" cols="30" rows="5" className="border-1 w-[60%]"></textarea>
+              <textarea name="description" id="" cols="30" rows="5" className="border-1 w-[80%]"></textarea>
             </div>
            
             <div className="flex items-center px-4">
               <Label inputName={'duration'} labelName={'duration'} className={'capitalize'}/>     
-              <input type="number" name="duration" id="" min={1} max={300} placeholder="in minutes" className="border-1 rounded-sm px-4 py-2 w-[60%]" />
+              <input type="number" name="duration" id="" min={1} max={300} placeholder="in minutes" className="border-1 rounded-sm px-4 py-2 w-[80%]" />
             </div>
            
             <div className="flex items-center px-4">
               <Label inputName={'ratings'} labelName={'ratings'} className={'capitalize'}/>     
-              <input type="number" name="ratings" id="" min={0.1} max={10.0} className="border-1 rounded-sm px-4 py-2 w-[60%]" placeholder="example: 8.1/10" />
+              <input type="number" name="ratings" id="" min={0.1} max={10.0} className="border-1 rounded-sm px-4 py-2 w-[80%]" placeholder="example: 8.1/10" />
             </div>
 
              <div className="flex items-center px-4">
                 <Label inputName={'releaseDate'} labelName={'release Date'} className={'capitalize'}/>     
-                <input type="date" name="releaseDate" id="releaseDate" min={'2025-02-01'} max={date} className="border-1 rounded-sm px-4 py-2 w-[60%]" />
+                <input type="date" name="releaseDate" id="releaseDate" min={'2025-02-01'} max={date} className="border-1 rounded-sm px-4 py-2 w-[80%]" />
             </div>
 
             <div className="flex items-center px-4">
                 <Label inputName={'releaseyear'} labelName={'release year'} className={'capitalize'}/>     
-                <input type="date" name="releasedate" id="" className="border-1 rounded-sm px-4 py-2 w-[60%]" />
+                <input type="date" name="releasedate" id="" className="border-1 rounded-sm px-4 py-2 w-[80%]" />
             </div>
             <div className="flex items-center px-4">
               <input type="file" className="bg-white" />
